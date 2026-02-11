a1187301340f2f62f8ecc65dfd2e7f8ff48b15c1
Nadirsha-Syed <nadirshasyed835@gmail.com>
2026-02-11 22:20:25 +0530

    Day 1: Backend setup and MongoDB Atlas integration completed

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Rental Platform API Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
